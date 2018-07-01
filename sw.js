c28db50d80c58ba305ba67eaafc9834b9e75706c
Iron Brandy <ironbrandy@IronnoMacBook-Air.local>
2018-07-01 19:51:49 +0900

    service worker

---
+++ sw.js
@@ -0,0 +1,58 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+	'/',
+	'/styles/main.css',
+	'/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+				if (response) {
+					return response;
+				}
+				var fetchRequest = event.request.clone();
+				
+				return fetch(event.request).then(
+					function(response) {
+						if (!response || response.status !== 200 || response.type !== 'basic') {
+							return response;
+						}
+						var responseToCache = response.clone();
+
+						caches.open(CACHE_NAME)
+							.then(function(cache) {
+								cache.put(event.request, responseToCache);
+						});
+						return response;
+					}
+				);
+			})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1' ];
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName) {
+					if (cacheWhitelist.IndexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
