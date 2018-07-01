02465b2fdcfb3557b6875b06c3db8cfbad98c4c5
Iron Brandy <ironbrandy@IronnoMacBook-Air.local>
2018-07-01 18:11:41 +0900

    service worker

--- 01.js
+++ 01.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(function(registration) {
-		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		console.log('ServiceWorker registration successful in scope: ', registration.scope);
 	}).catch(function(err) {
 		console.log('ServiceWorker registration failed: ', err);
 	});
